5534c325326194622dd8ff6c51ebde3c27962fde
Anbar Saleem <anbar_saleem@colpal.com>
2023-01-27 14:21:23 -0500

    added html grid and successfully requested images from pixabay api

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [images, setImages] = useState([] as any[]);
+  const API_KEY = "33167824-361d207c5e720e0f6b5e1d91d";
+  useEffect(() => {
+    axios.get(
+      `https://pixabay.com/api/?key=${API_KEY}&q=cats&image_type=photo&per_page=9`
+    ).then((res) => setImages(res.data.hits));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className={`${'grid grid-cols-3'} gap-4 m-4`}
+    >
+      {images.map((image) => (
+        <div key = {image.id}>
+          <img
+            src = {image.webformatURL}
+            alt = {image.tags}
+          />
+          <p className = "text-center font-medium text-gray-700">
+            {image.tags.split(',')[0]}
+          </p>
+        </div>
+      ))}
     </div>
   );
 }
